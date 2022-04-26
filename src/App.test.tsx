5fcc8f5b19957f844a0849140d0e2f57471d8fa5
Divya <divyabhatt16@gmail.com>
2022-04-26 02:05:04 +0100

    Testing API calls in React with the Studio Ghibli API

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import { server } from "./mocks/server";
+import App from "./App";
+import { filmError500Task } from "./mocks/handlers";
+import { filmError418Task } from "./mocks/handlers";
+import { rest } from "msw";
 
-test('renders learn react link', () => {
+test("loads and displays films", async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const filmsData = await screen.findAllByTestId("filmDataId");
+  expect(screen.getAllByTestId("filmDataId")[0]).toHaveTextContent(
+    "Castle in the Sky"
+  );
+});
+
+test("handles server error 500", async () => {
+  server.use(filmError500Task);
+  render(<App />);
+  const errorData = await screen.findAllByTestId("filmErrorId");
+  expect(screen.getAllByTestId("filmErrorId")[0]).toHaveTextContent(
+    "Oops… something went wrong, try again 🤕"
+  );
+});
+
+test("handles server error 418", async () => {
+  server.use(filmError418Task);
+  render(<App />);
+  const errorData = await screen.findAllByTestId("filmErrorId");
+  expect(screen.getAllByTestId("filmErrorId")[0]).toHaveTextContent(
+    "418:I'm a tea pot 🫖, silly"
+  );
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [films, setFilms] = useState([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("https://ghibliapi.herokuapp.com/films")
+      .then((response) => {
+        const { data } = response;
+        setFilms(data);
+      })
+      .catch((error) => {
+        if (JSON.stringify(error.message).includes("500")) {
+          error = "Oops… something went wrong, try again 🤕";
+        } else if (JSON.stringify(error.message).includes("418")) {
+          error = "418:I'm a tea pot 🫖, silly";
+        }
+        setError(error);
+      });
+  }, []);
+
   return (
-    <div className="App">
+    <div data-testid="App" className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {error && <p data-testid="filmErrorId">{JSON.stringify(error)}</p>}
+        {films &&
+          films.map((film) => (
+            <p data-testid="filmDataId">{JSON.stringify(film.title)}</p>
+          ))}
       </header>
     </div>
   );
